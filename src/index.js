aa2c1e51df16ff426a3f1c04ccc173c60133d6f1
Prashan Prajapati <officialprashan@gmail.com>
2025-01-31 14:56:18 +0545

    net-3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-const path = require("path");
-const http = require("http");
 const express = require("express");
+const http = require("http");
 const socketio = require("socket.io");
 const serverless = require("serverless-http");
 const Filter = require("bad-words");
+const path = require("path");
+
 const {
   generateMessage,
   generateLocationMessage,
@@ -18,24 +19,25 @@ const {
 const app = express();
 const router = express.Router();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 const publicDirectoryPath = path.join(__dirname, "../public");
-
 app.use(express.static(publicDirectoryPath));
 
 io.on("connection", (socket) => {
-  console.log("New Websocket Connection");
+  console.log("New WebSocket connection");
 
   socket.on("join", (options, callback) => {
     const { error, user } = addUser({ id: socket.id, ...options });
 
-    if (error) {
-      return callback(error);
-    }
+    if (error) return callback(error);
 
     socket.join(user.room);
-
     socket.emit("message", generateMessage("Admin", "Welcome!"));
     socket.broadcast
       .to(user.room)
@@ -43,17 +45,19 @@ io.on("connection", (socket) => {
         "message",
         generateMessage("Admin", `${user.username} has joined!`)
       );
+
     io.to(user.room).emit("roomData", {
       room: user.room,
       users: getUsersInRoom(user.room),
     });
+
     callback();
   });
 
   socket.on("sendMessage", (message, callback) => {
     const user = getUser(socket.id);
-
     const filter = new Filter();
+
     if (filter.isProfane(message)) {
       return callback("Profanity is not allowed");
     }
